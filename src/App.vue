46f257aab073b767a6b9739839be0e8d2b5fec32
huchanghao <2955568032@qq.com>
2022-11-25 00:59:14 +0800

    feat: configure commitlint

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <button @click="increment">{{ count }}</button>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+const increment = () => count.value++
+</script>
 
 <style lang="less"></style>
